6411d96e0b50af93dc7c8c5a946866087d029008
Xavier Rettel <xavier@ret.tel>
2021-08-30 22:22:24 +0200

    Add pollution episodes to sensor

--- custom_components/airparif/api.py
+++ custom_components/airparif/api.py
@@ -12,9 +12,11 @@ TIMEOUT = 10
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
+API_ROOT = "https://api.airparif.asso.fr"
+
 VALUE_ENUM = ["Bon", "Moyen", "Dégradé", "Mauvais", "Très Mauvais", "Extrêmement Mauvais"]
 
-SCHEMA = {
+SCHEMA_PREVISIONS = {
     "type": "object",
     "patternProperties": {
         "^\\d{5}$": {
@@ -42,6 +44,42 @@ SCHEMA = {
     "minProperties": 1
 }
 
+SCHEMA_EPISODES = {
+    "type": "object",
+    "properties": {
+        "actif": {"type": "boolean"},
+        "message": {"type": "object", "properties": {"fr": {"type": "string"}, "en": {"type": "string"}}},
+        "jour": {
+            "type": "object",
+            "properties": {
+                "actif": {"type": "boolean"},
+                "polluants": {"type": "array",
+                              "items": {
+                                  "type": "object",
+                                  "properties": {
+                                      "nom": {"type": "string"},
+                                      "niveau": {"type": "string"}
+                                  }
+                              }}
+            }
+        },
+        "demain": {
+            "type": "object",
+            "properties": {
+                "actif": {"type": "boolean"},
+                "polluants": {"type": "array",
+                              "items": {
+                                  "type": "object",
+                                  "properties": {
+                                      "nom": {"type": "string"},
+                                      "niveau": {"type": "string"}
+                                  }
+                              }}
+            }
+        }
+    }
+}
+
 
 class AirparifApiClient:
     def __init__(self, api_key: str, session: aiohttp.ClientSession) -> None:
@@ -50,23 +88,15 @@ class AirparifApiClient:
         self._session = session
         self._headers = {"Content-type": f"application/json; charset=UTF-8;", "X-Api-Key": f"{api_key}"}
 
-    async def async_get_previsions(self, insee: str) -> list:
-        """Get air quality prevision data for a city based on its INSEE code."""
-        url = f"https://api.airparif.asso.fr/indices/prevision/commune?insee={insee}"
+    async def api_wrapper(self, url: str) -> dict:
+        """Get information from the API."""
         try:
             async with async_timeout.timeout(TIMEOUT, loop=asyncio.get_event_loop()):
-                response = await self._session.get(url, headers=self._headers)
-                if response.status == 200:
-                    r = await response.json()
-                    if r == {}:
-                        raise ValueError(f"City {insee} does not exist or INSEE code is invalid")
-                    else:
-                        if jsonschema.Draft7Validator(SCHEMA).is_valid(r):
-                            return sorted(r[str(insee)], key=lambda x: datetime.strptime(x['date'], "%Y-%m-%d"))
-                        else:
-                            raise ValueError("Invalid data received from Airparif API")
-                elif response.status == 403:
-                    raise PermissionError
+                response = await self._session.get(f"{API_ROOT}{url}", headers=self._headers)
+                if response.status == 403:
+                    raise PermissionError("Access denied (403)")
+                elif response.status == 200:
+                    return await response.json()
                 else:
                     raise ValueError(f"Invalid request: {response.status}")
 
@@ -75,8 +105,49 @@ class AirparifApiClient:
         except (aiohttp.ClientError, socket.gaierror) as exception:
             _LOGGER.error("Error fetching information from %s - %s", url, exception)
         except PermissionError as exception:
-            _LOGGER.error("Invalid API key %s", self._api_key)
+            _LOGGER.error("Permission error - %s", exception)
         except (KeyError, TypeError) as exception:
             _LOGGER.error("Error parsing information from %s - %s", url, exception)
         except Exception as exception:  # pylint: disable=broad-except
             _LOGGER.error("Something really wrong happened! - %s", exception)
+
+    async def async_get_version(self) -> str:
+        """Get Airparif API version."""
+        url = f"/version"
+        response = await self.api_wrapper(url)
+        return response["version"]
+
+    async def async_get_previsions(self, insee: str) -> dict:
+        """Get air quality prevision data for a city based on its INSEE code."""
+        url = f"/indices/prevision/commune?insee={insee}"
+        response = await self.api_wrapper(url)
+        if response == {}:
+            _LOGGER.error(f"City {insee} does not exist or INSEE code is invalid")
+            return {"today": {}, "tomorrow": {}}
+        else:
+            if jsonschema.Draft7Validator(SCHEMA_PREVISIONS).is_valid(response):
+                p = sorted(response[str(insee)], key=lambda x: datetime.strptime(x['date'], "%Y-%m-%d"))
+                return {"today": p[0], "tomorrow": p[1] if len(p) == 2 else {}}
+            else:
+                _LOGGER.error("Received invalid previsions data from Airparif API")
+                return {"today": {}, "tomorrow": {}}
+
+    async def async_get_episodes(self) -> dict:
+        """Get Airparif polution episodes previsions."""
+        url = f"/episodes/en-cours-et-prevus"
+        response = await self.api_wrapper(url)
+        if jsonschema.Draft7Validator(SCHEMA_EPISODES).is_valid(response):
+            return {"today": {"episode": response["jour"]["actif"]},
+                    "tomorrow": {"episode": response["demain"]["actif"]}}
+        else:
+            _LOGGER.error("Received invalid previsions data from Airparif API")
+            return {"today": {}, "tomorrow": {}}
+
+    async def async_get_data(self, insee: str) -> dict:
+        #TODO: Rework exceptions handling
+        previsions = await self.async_get_previsions(insee)
+        episodes = await self.async_get_episodes()
+        response = {"today": previsions["today"], "tomorrow": previsions["tomorrow"]}
+        response["today"].update(episodes["today"])
+        response["tomorrow"].update(episodes["tomorrow"])
+        return response
--- custom_components/airparif/sensor.py
+++ custom_components/airparif/sensor.py
@@ -15,7 +15,6 @@ from homeassistant.const import (
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
-from homeassistant.helpers.typing import StateType
 
 from .api import AirparifApiClient
 
@@ -26,8 +25,10 @@ ATTR_OZONE = "ozone"
 ATTR_PM10 = "pm_10"
 ATTR_PM2_5 = "pm_2_5"
 ATTR_SULFUR_DIOXIDE = "sulfur_dioxide"
-ATTR_tomorrow = "tomorrow"
+ATTR_TOMORROW = "tomorrow"
+ATTR_TODAY = "today"
 ATTR_AQI = "aqi"
+ATTR_EPISODE = "episode"
 
 KEY_TO_ATTR = {
     "date": ATTR_DATE,
@@ -36,7 +37,8 @@ KEY_TO_ATTR = {
     "o3": ATTR_OZONE,
     "no2": ATTR_NITROGEN_DIOXIDE,
     "so2": ATTR_SULFUR_DIOXIDE,
-    "indice": ATTR_AQI
+    "indice": ATTR_AQI,
+    "episode": ATTR_EPISODE
 }
 
 ATTRIBUTION = "Data provided by Airparif"
@@ -93,16 +95,15 @@ class AirparifSensor(SensorEntity):
     @property
     def icon(self):
         """Icon to use in the frontend, if any."""
-        return "mdi:cloud"
+        return "mdi:blur"
 
     @property
-    def state(self) -> StateType:
-        return self._data[0]["indice"]
-
-    @property
-    def native_value(self):
+    def state(self):
         """Return the state of the device."""
-        return self._data[0]["indice"]
+        if self._data is not None and "indice" in self._data[ATTR_TODAY].keys():
+            return self._data["today"]["indice"]
+        else:
+            return None
 
     @property
     def available(self):
@@ -117,17 +118,16 @@ class AirparifSensor(SensorEntity):
     @property
     def extra_state_attributes(self):
         """Return the state attributes of the last update."""
-        attrs = {"today": {}, "tomorrow": {}, ATTR_ATTRIBUTION: ATTRIBUTION}
+        attrs = {ATTR_TODAY: {}, ATTR_TOMORROW: {}, ATTR_ATTRIBUTION: ATTRIBUTION}
         if self._data is not None:
             try:
                 for (k, attr) in KEY_TO_ATTR.items():
-                    attrs["today"][attr] = self._data[0][k]
-                    if len(self._data) == 2:
-                        attrs["tomorrow"][attr] = self._data[1][k]
+                    attrs[ATTR_TODAY][attr] = self._data["today"][k]
+                    attrs[ATTR_TOMORROW][attr] = self._data["tomorrow"][k]
                 return attrs
             except (IndexError, KeyError):
                 return {ATTR_ATTRIBUTION: ATTRIBUTION}
 
     async def async_update(self):
         """Get the latest data and updates the states."""
-        self._data = await self._client.async_get_previsions(self._insee)
+        self._data = await self._client.async_get_data(self._insee)
